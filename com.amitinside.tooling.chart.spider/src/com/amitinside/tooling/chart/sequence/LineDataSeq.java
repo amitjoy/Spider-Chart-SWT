87b3d8984afe0d952d4ae23e0ab199e4a6272a2f
Amit Kumar Mondal <admin@amitinside.com>
2016-03-15 14:37:15 +0100

    Added more comments

--- com.amitinside.tooling.chart.spider/src/com/amitinside/tooling/chart/sequence/LineDataSeq.java
+++ com.amitinside.tooling.chart.spider/src/com/amitinside/tooling/chart/sequence/LineDataSeq.java
@@ -55,21 +55,28 @@ public final class LineDataSeq extends DataSeq {
 		return seq;
 	}
 
-	/** */
+	/** Draw Point Configuration */
 	private boolean drawPoint = false;
-	/** */
+
+	/** Fillstyle to be used */
 	private FillStyle fillStyle = null;
-	/** */
+
+	/** Icon to be used */
 	private AbstractChartImage icon = null;
-	/** */
+
+	/** Line Type */
 	private int lineType = 0;
-	/** */
+
+	/** Data Point Color */
 	private AbstractChartColor pointColor = getColor(BLACK);
-	/** */
+
+	/** Line Style */
 	private LineStyle style = null;
-	/** */
+
+	/** value Color */
 	private AbstractChartColor valueColor = getColor(BLACK);
-	/** */
+
+	/** value font */
 	private AbstractChartFont valueFont = null;
 
 	/** */
